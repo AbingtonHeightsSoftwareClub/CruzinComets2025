8263829e2fb1bd40756b37f660d06b27b7b3aeb9
smallison3409@gmail.com <smallison3409@gmail.com>
2026-01-02 12:18:53 -0500

    Tried to integrate NextFTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTCtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTCtest.java
@@ -1,5 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
-public class nextFTCtest {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.mechanisms.Constants;
+
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.extensions.pedro.PedroDriverControlled;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.hardware.driving.DriverControlledCommand;
+
+@TeleOp
+public class nextFTCtest extends NextFTCOpMode {
+
+
+    DriverControlledCommand driverControlled = new PedroDriverControlled(
+            Gamepads.gamepad1().leftStickY(),
+            Gamepads.gamepad1().leftStickX(),
+            Gamepads.gamepad1().rightStickX()
+    );
+
+
+
+    {
+        addComponents(
+                new PedroComponent(Constants::createFollower)
+        );
+    }
+
+    @Override public void onInit() {
+        driverControlled.schedule();
+    }
+    @Override public void onWaitForStart() { }
+    @Override public void onStartButtonPressed() { }
+    @Override public void onUpdate() {
+
+    }
+    @Override public void onStop() { }
 }
